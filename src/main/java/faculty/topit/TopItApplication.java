c0ec67efac1cdfc8fe468dba6cb7ac396d82aa40
janina280 <coceijanina@yahoo.com>
2024-10-07 23:22:00 +0300

    create project

---
+++ src/main/java/faculty/topit/TopItApplication.java
@@ -0,0 +1,13 @@
+package faculty.topit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/faculty/topit/TopItApplicationTests.java
@@ -0,0 +1,13 @@
+package faculty.topit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
