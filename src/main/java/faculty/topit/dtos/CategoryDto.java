1311ae347bb7b367bbf496e4618265feeecd37da
janina280 <coceijanina@yahoo.com>
2024-10-09 09:30:47 +0300

    category dto

--- src/main/java/faculty/topit/dtos/CategoryDto.java
+++ src/main/java/faculty/topit/dtos/CategoryDto.java
@@ -1,5 +1,13 @@
 package faculty.topit.dtos;
 
+import faculty.topit.models.CategoryModel;
+import lombok.Data;
+
+@Data
 public class CategoryDto {
     private String name;
+
+    public CategoryDto(CategoryModel model) {
+        this.name = model.getName();
+    }
 }
--- src/main/java/faculty/topit/services/CategoryService.java
+++ src/main/java/faculty/topit/services/CategoryService.java
@@ -1,22 +1,32 @@
 package faculty.topit.services;
 
 import faculty.topit.dtos.CategoryDto;
-import faculty.topit.models.CategoryModel;
 import faculty.topit.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     private CategoryRepository categoryRepository;
     @Autowired
     public CategoryService(CategoryRepository repository){
+
         categoryRepository=repository;
     }
     public List<CategoryDto> getAllCategories(){
         var categories= categoryRepository.findAll();
-        
+        return categories.stream().map(CategoryDto::new).collect(Collectors.toList());
+    }
+
+    public CategoryDto getCategoryByName(String name){
+        var category= categoryRepository.getReferenceById(name);
+        return new CategoryDto(category);
+    }
+
+    public void deleteCategory(String name) {
+        categoryRepository.deleteById(name);
     }
 }
